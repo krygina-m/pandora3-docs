e28091899ab2962db67589c2a5fc429987bd7cf7
NesCafe62 <U.238@rambler.ru>
2019-02-20 20:12:50 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
-    base: '/pandora3/'
-    // ...
+    base: '/pandora3/',
+    themeConfig: {
+        nav: [
+            {text: 'Быстрый старт',     link: '/getting-starged.html'},
+            {text: 'Основы',            link: '/basics.html'}
+        ]
+        /* nav: [{
+            text: 'Быстрый старт',
+            items: [
+                {text: 'Chinese', link: '/language/chinese/'},
+                {text: 'Japanese', link: '/language/japanese/'}
+            ]
+        }] */
+    }
 };
