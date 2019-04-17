df4143c1dbecb094896aab3c49ed9ea0c2e81d66
NesCafe62 <U.238@rambler.ru>
2019-04-17 15:44:10 +0300

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,22 +4,13 @@ module.exports = {
         nav: [
             {text: 'Быстрый старт',     link: '/getting-started.html'},
             {text: 'Основы',            link: '/basics.html'},
-            {text: 'API',               link: '/api/classes.html'},
+            {text: 'API',               link: '/api.html'},
             {text: 'Концепция',         link: '/concept.html'}
         ],
         sidebar: [
             '/getting-started',
             '/basics',
-            {
-                title: 'API',
-                children: [
-                    '/api/classes.html',
-                    '/api/interfaces.html',
-                    '/api/constants.html',
-                    '/api/functions.html',
-                    '/api/traits.html'
-                ]
-            },
+            '/api',
             '/concept'
         ]
         /* nav: [{
