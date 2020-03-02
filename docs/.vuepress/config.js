6cb3cb007d7896a5dc6a9e3a1a88ad92424c871b
NesCafe62 <1944556+NesCafe62@users.noreply.github.com>
2020-03-02 23:57:47 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,36 @@
 module.exports = {
-    base: '/pandora3/',
-    themeConfig: {
-        repo: 'PandoraTeam/pandora3-Core',
-        docsRepo: 'PandoraTeam/pandora3-docs',
-        docsDir: 'docs',
-        editLinks: true,
-        lastUpdated: true,
-        
-        nav: [
-            {text: 'Быстрый старт',     link: '/getting-started.html'},
-            {text: 'Основы',            link: '/basics.html'},
-            {text: 'API',               link: '/api/'},
-            {text: 'Концепция',         link: '/concept.html'}
-        ],
-        sidebarDepth: 2,
-        sidebar: [
-            '/getting-started',
-            '/basics',
-            ['/api/', 'API'],
-            '/concept'
-        ]
-        /* nav: [{
-            text: 'Быстрый старт',
-            items: [
-                {text: 'Chinese', link: '/language/chinese/'},
-                {text: 'Japanese', link: '/language/japanese/'}
-            ]
-        }] */
-    }
+	base: '/pandora3/',
+	title: 'Pandora 3',
+	description: 'Lightweight PHP framework',
+	head: [
+		['link', { rel: 'icon', href: '/logo.png' }]
+	],
+	themeConfig: {
+		repo: 'PandoraTeam/pandora3-Core',
+		docsRepo: 'PandoraTeam/pandora3-docs',
+		docsDir: 'docs',
+		editLinks: true,
+		lastUpdated: true,
+		
+		nav: [
+			{text: 'Быстрый старт',	link: '/getting-started.html'},
+			{text: 'Основы',		link: '/basics.html'},
+			{text: 'API',			link: '/api/'},
+			{text: 'Концепция',		link: '/concept.html'}
+		],
+		sidebarDepth: 2,
+		sidebar: [
+			'/getting-started',
+			'/basics',
+			['/api/', 'API'],
+			'/concept'
+		]
+		/* nav: [{
+			text: 'Быстрый старт',
+			items: [
+				{text: 'Chinese',	link: '/language/chinese/'},
+				{text: 'Japanese',	link: '/language/japanese/'}
+			]
+		}] */
+	}
 };
